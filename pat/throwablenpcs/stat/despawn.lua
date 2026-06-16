03ea2f630724ac27612f891dc12758c52090b440
patmanf <27874300+patmanf@users.noreply.github.com>
2026-06-16 00:40:27 -0500

    despawn works better probably?

--- pat/throwablenpcs/stat/despawn.lua
+++ pat/throwablenpcs/stat/despawn.lua
@@ -1,9 +1,11 @@
 function init()
+  if entity.entityType() ~= "npc" then return end
+  effect.modifyDuration(10)
+  
   local id = entity.id()
-  if world.entityType(id) ~= "npc" then return end
-
   world.callScriptedEntity(id, "npc.setDeathParticleBurst")
-  mcontroller.setYPosition(-67)
+  world.callScriptedEntity(id, "npc.resetLounging")
   effect.setParentDirectives("?multiply=0000")
   effect.addStatModifierGroup({ { stat = "maxHealth", effectiveMultiplier = 0 } })
+  mcontroller.setYPosition(-67)
 end
