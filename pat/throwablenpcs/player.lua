db1baba6ebed22a1947952936cf901807470c4dd
patmanf <27874300+patmanf@users.noreply.github.com>
2026-06-24 19:44:38 -0500

    osb keybind to toggle quest tracking

--- pat/throwablenpcs/player.lua
+++ pat/throwablenpcs/player.lua
@@ -1,6 +1,24 @@
+local questId = "pat_throwablenpc"
+
 function init()
-  script.setUpdateDelta(0)
-  if not player.hasActiveQuest("pat_throwablenpc") then
-    player.startQuest("pat_throwablenpc")
+  if not player.hasActiveQuest(questId) then
+    player.startQuest(questId)
+  end
+
+  if not input or not player.trackedQuestId or not player.setTrackedQuest then
+    script.setUpdateDelta(0)
+  end
+end
+
+function update()
+  if input.bindDown("pat_throwablenpcs", "toggle") then
+    local current = player.trackedQuestId()
+    if current == questId then
+      player.setTrackedQuest(storage.lastQuestId or "")
+      storage.lastQuestId = nil
+    else
+      player.setTrackedQuest(questId)
+      storage.lastQuestId = current
+    end
   end
 end
